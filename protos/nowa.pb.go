97948c17f7cca995cb0396c9a9080c57af152457
booneng <ohbooneng@gmail.com>
2020-07-12 12:28:45 +0800

    regenerate nowa proto

--- protos/nowa.pb.go
+++ protos/nowa.pb.go
@@ -25,6 +25,107 @@ const (
 // of the legacy proto package is being used.
 const _ = proto.ProtoPackageIsVersion4
 
+type MenuItem_Status int32
+
+const (
+	MenuItem_UNKNOWN  MenuItem_Status = 0
+	MenuItem_ACTIVE   MenuItem_Status = 1
+	MenuItem_SOLD_OUT MenuItem_Status = 2
+)
+
+// Enum value maps for MenuItem_Status.
+var (
+	MenuItem_Status_name = map[int32]string{
+		0: "UNKNOWN",
+		1: "ACTIVE",
+		2: "SOLD_OUT",
+	}
+	MenuItem_Status_value = map[string]int32{
+		"UNKNOWN":  0,
+		"ACTIVE":   1,
+		"SOLD_OUT": 2,
+	}
+)
+
+func (x MenuItem_Status) Enum() *MenuItem_Status {
+	p := new(MenuItem_Status)
+	*p = x
+	return p
+}
+
+func (x MenuItem_Status) String() string {
+	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
+}
+
+func (MenuItem_Status) Descriptor() protoreflect.EnumDescriptor {
+	return file_protos_nowa_proto_enumTypes[0].Descriptor()
+}
+
+func (MenuItem_Status) Type() protoreflect.EnumType {
+	return &file_protos_nowa_proto_enumTypes[0]
+}
+
+func (x MenuItem_Status) Number() protoreflect.EnumNumber {
+	return protoreflect.EnumNumber(x)
+}
+
+// Deprecated: Use MenuItem_Status.Descriptor instead.
+func (MenuItem_Status) EnumDescriptor() ([]byte, []int) {
+	return file_protos_nowa_proto_rawDescGZIP(), []int{1, 0}
+}
+
+type Order_Status int32
+
+const (
+	Order_UNKNOWN   Order_Status = 0
+	Order_ACTIVE    Order_Status = 1
+	Order_COMPLETED Order_Status = 2
+	Order_CANCELLED Order_Status = 3
+)
+
+// Enum value maps for Order_Status.
+var (
+	Order_Status_name = map[int32]string{
+		0: "UNKNOWN",
+		1: "ACTIVE",
+		2: "COMPLETED",
+		3: "CANCELLED",
+	}
+	Order_Status_value = map[string]int32{
+		"UNKNOWN":   0,
+		"ACTIVE":    1,
+		"COMPLETED": 2,
+		"CANCELLED": 3,
+	}
+)
+
+func (x Order_Status) Enum() *Order_Status {
+	p := new(Order_Status)
+	*p = x
+	return p
+}
+
+func (x Order_Status) String() string {
+	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
+}
+
+func (Order_Status) Descriptor() protoreflect.EnumDescriptor {
+	return file_protos_nowa_proto_enumTypes[1].Descriptor()
+}
+
+func (Order_Status) Type() protoreflect.EnumType {
+	return &file_protos_nowa_proto_enumTypes[1]
+}
+
+func (x Order_Status) Number() protoreflect.EnumNumber {
+	return protoreflect.EnumNumber(x)
+}
+
+// Deprecated: Use Order_Status.Descriptor instead.
+func (Order_Status) EnumDescriptor() ([]byte, []int) {
+	return file_protos_nowa_proto_rawDescGZIP(), []int{2, 0}
+}
+
 type Restaurant struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -34,6 +135,8 @@ type Restaurant struct {
 	RestaurantId int32 `protobuf:"varint,1,opt,name=restaurant_id,json=restaurantId,proto3" json:"restaurant_id,omitempty"`
 	// Name of the restaurant.
 	Name string `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
+	// List of menu items belonging to this restaurant.
+	MenuItems []*MenuItem `protobuf:"bytes,3,rep,name=menu_items,json=menuItems,proto3" json:"menu_items,omitempty"`
 }
 
 func (x *Restaurant) Reset() {
@@ -82,18 +185,278 @@ func (x *Restaurant) GetName() string {
 	return ""
 }
 
+func (x *Restaurant) GetMenuItems() []*MenuItem {
+	if x != nil {
+		return x.MenuItems
+	}
+	return nil
+}
+
+type MenuItem struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Unique identifier for the menu item.
+	MenuId int64 `protobuf:"varint,1,opt,name=menu_id,json=menuId,proto3" json:"menu_id,omitempty"`
+	// Status of the menu item.
+	Status MenuItem_Status `protobuf:"varint,2,opt,name=status,proto3,enum=nowa.MenuItem_Status" json:"status,omitempty"`
+	// Name of the menu item.
+	Name string `protobuf:"bytes,3,opt,name=name,proto3" json:"name,omitempty"`
+	// Description of the menu item.
+	Description string `protobuf:"bytes,4,opt,name=description,proto3" json:"description,omitempty"`
+}
+
+func (x *MenuItem) Reset() {
+	*x = MenuItem{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_protos_nowa_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *MenuItem) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*MenuItem) ProtoMessage() {}
+
+func (x *MenuItem) ProtoReflect() protoreflect.Message {
+	mi := &file_protos_nowa_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use MenuItem.ProtoReflect.Descriptor instead.
+func (*MenuItem) Descriptor() ([]byte, []int) {
+	return file_protos_nowa_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *MenuItem) GetMenuId() int64 {
+	if x != nil {
+		return x.MenuId
+	}
+	return 0
+}
+
+func (x *MenuItem) GetStatus() MenuItem_Status {
+	if x != nil {
+		return x.Status
+	}
+	return MenuItem_UNKNOWN
+}
+
+func (x *MenuItem) GetName() string {
+	if x != nil {
+		return x.Name
+	}
+	return ""
+}
+
+func (x *MenuItem) GetDescription() string {
+	if x != nil {
+		return x.Description
+	}
+	return ""
+}
+
+type Order struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Unique identifier for the order.
+	OrderId int64 `protobuf:"varint,1,opt,name=order_id,json=orderId,proto3" json:"order_id,omitempty"`
+	// ID of the user who placed the order.
+	UserId int64 `protobuf:"varint,2,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
+	// ID of the restaurant that the order is for.
+	RestaurantId int32 `protobuf:"varint,3,opt,name=restaurant_id,json=restaurantId,proto3" json:"restaurant_id,omitempty"`
+	// Status of the order.
+	Status Order_Status `protobuf:"varint,4,opt,name=status,proto3,enum=nowa.Order_Status" json:"status,omitempty"`
+	// List of items in this order.
+	OrderItem []*Order_OrderItem `protobuf:"bytes,5,rep,name=order_item,json=orderItem,proto3" json:"order_item,omitempty"`
+}
+
+func (x *Order) Reset() {
+	*x = Order{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_protos_nowa_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Order) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Order) ProtoMessage() {}
+
+func (x *Order) ProtoReflect() protoreflect.Message {
+	mi := &file_protos_nowa_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Order.ProtoReflect.Descriptor instead.
+func (*Order) Descriptor() ([]byte, []int) {
+	return file_protos_nowa_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *Order) GetOrderId() int64 {
+	if x != nil {
+		return x.OrderId
+	}
+	return 0
+}
+
+func (x *Order) GetUserId() int64 {
+	if x != nil {
+		return x.UserId
+	}
+	return 0
+}
+
+func (x *Order) GetRestaurantId() int32 {
+	if x != nil {
+		return x.RestaurantId
+	}
+	return 0
+}
+
+func (x *Order) GetStatus() Order_Status {
+	if x != nil {
+		return x.Status
+	}
+	return Order_UNKNOWN
+}
+
+func (x *Order) GetOrderItem() []*Order_OrderItem {
+	if x != nil {
+		return x.OrderItem
+	}
+	return nil
+}
+
+type Order_OrderItem struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Menu item.
+	MenuItem *MenuItem `protobuf:"bytes,1,opt,name=menu_item,json=menuItem,proto3" json:"menu_item,omitempty"`
+	// Quantity of the menu_item.
+	Quantity int32 `protobuf:"varint,2,opt,name=quantity,proto3" json:"quantity,omitempty"`
+}
+
+func (x *Order_OrderItem) Reset() {
+	*x = Order_OrderItem{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_protos_nowa_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Order_OrderItem) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Order_OrderItem) ProtoMessage() {}
+
+func (x *Order_OrderItem) ProtoReflect() protoreflect.Message {
+	mi := &file_protos_nowa_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Order_OrderItem.ProtoReflect.Descriptor instead.
+func (*Order_OrderItem) Descriptor() ([]byte, []int) {
+	return file_protos_nowa_proto_rawDescGZIP(), []int{2, 0}
+}
+
+func (x *Order_OrderItem) GetMenuItem() *MenuItem {
+	if x != nil {
+		return x.MenuItem
+	}
+	return nil
+}
+
+func (x *Order_OrderItem) GetQuantity() int32 {
+	if x != nil {
+		return x.Quantity
+	}
+	return 0
+}
+
 var File_protos_nowa_proto protoreflect.FileDescriptor
 
 var file_protos_nowa_proto_rawDesc = []byte{
 	0x0a, 0x11, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2f, 0x6e, 0x6f, 0x77, 0x61, 0x2e, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x12, 0x04, 0x6e, 0x6f, 0x77, 0x61, 0x22, 0x45, 0x0a, 0x0a, 0x52, 0x65, 0x73,
+	0x6f, 0x74, 0x6f, 0x12, 0x04, 0x6e, 0x6f, 0x77, 0x61, 0x22, 0x74, 0x0a, 0x0a, 0x52, 0x65, 0x73,
 	0x74, 0x61, 0x75, 0x72, 0x61, 0x6e, 0x74, 0x12, 0x23, 0x0a, 0x0d, 0x72, 0x65, 0x73, 0x74, 0x61,
 	0x75, 0x72, 0x61, 0x6e, 0x74, 0x5f, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x0c,
 	0x72, 0x65, 0x73, 0x74, 0x61, 0x75, 0x72, 0x61, 0x6e, 0x74, 0x49, 0x64, 0x12, 0x12, 0x0a, 0x04,
 	0x6e, 0x61, 0x6d, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65,
-	0x42, 0x20, 0x5a, 0x1e, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x62,
-	0x6f, 0x6f, 0x6e, 0x65, 0x6e, 0x67, 0x2f, 0x6e, 0x6f, 0x77, 0x61, 0x2f, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x12, 0x2d, 0x0a, 0x0a, 0x6d, 0x65, 0x6e, 0x75, 0x5f, 0x69, 0x74, 0x65, 0x6d, 0x73, 0x18, 0x03,
+	0x20, 0x03, 0x28, 0x0b, 0x32, 0x0e, 0x2e, 0x6e, 0x6f, 0x77, 0x61, 0x2e, 0x4d, 0x65, 0x6e, 0x75,
+	0x49, 0x74, 0x65, 0x6d, 0x52, 0x09, 0x6d, 0x65, 0x6e, 0x75, 0x49, 0x74, 0x65, 0x6d, 0x73, 0x22,
+	0xb9, 0x01, 0x0a, 0x08, 0x4d, 0x65, 0x6e, 0x75, 0x49, 0x74, 0x65, 0x6d, 0x12, 0x17, 0x0a, 0x07,
+	0x6d, 0x65, 0x6e, 0x75, 0x5f, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x06, 0x6d,
+	0x65, 0x6e, 0x75, 0x49, 0x64, 0x12, 0x2d, 0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18,
+	0x02, 0x20, 0x01, 0x28, 0x0e, 0x32, 0x15, 0x2e, 0x6e, 0x6f, 0x77, 0x61, 0x2e, 0x4d, 0x65, 0x6e,
+	0x75, 0x49, 0x74, 0x65, 0x6d, 0x2e, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73, 0x52, 0x06, 0x73, 0x74,
+	0x61, 0x74, 0x75, 0x73, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x03, 0x20, 0x01,
+	0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x20, 0x0a, 0x0b, 0x64, 0x65, 0x73, 0x63,
+	0x72, 0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x18, 0x04, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0b, 0x64,
+	0x65, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x22, 0x2f, 0x0a, 0x06, 0x53, 0x74,
+	0x61, 0x74, 0x75, 0x73, 0x12, 0x0b, 0x0a, 0x07, 0x55, 0x4e, 0x4b, 0x4e, 0x4f, 0x57, 0x4e, 0x10,
+	0x00, 0x12, 0x0a, 0x0a, 0x06, 0x41, 0x43, 0x54, 0x49, 0x56, 0x45, 0x10, 0x01, 0x12, 0x0c, 0x0a,
+	0x08, 0x53, 0x4f, 0x4c, 0x44, 0x5f, 0x4f, 0x55, 0x54, 0x10, 0x02, 0x22, 0xd9, 0x02, 0x0a, 0x05,
+	0x4f, 0x72, 0x64, 0x65, 0x72, 0x12, 0x19, 0x0a, 0x08, 0x6f, 0x72, 0x64, 0x65, 0x72, 0x5f, 0x69,
+	0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x07, 0x6f, 0x72, 0x64, 0x65, 0x72, 0x49, 0x64,
+	0x12, 0x17, 0x0a, 0x07, 0x75, 0x73, 0x65, 0x72, 0x5f, 0x69, 0x64, 0x18, 0x02, 0x20, 0x01, 0x28,
+	0x03, 0x52, 0x06, 0x75, 0x73, 0x65, 0x72, 0x49, 0x64, 0x12, 0x23, 0x0a, 0x0d, 0x72, 0x65, 0x73,
+	0x74, 0x61, 0x75, 0x72, 0x61, 0x6e, 0x74, 0x5f, 0x69, 0x64, 0x18, 0x03, 0x20, 0x01, 0x28, 0x05,
+	0x52, 0x0c, 0x72, 0x65, 0x73, 0x74, 0x61, 0x75, 0x72, 0x61, 0x6e, 0x74, 0x49, 0x64, 0x12, 0x2a,
+	0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18, 0x04, 0x20, 0x01, 0x28, 0x0e, 0x32, 0x12,
+	0x2e, 0x6e, 0x6f, 0x77, 0x61, 0x2e, 0x4f, 0x72, 0x64, 0x65, 0x72, 0x2e, 0x53, 0x74, 0x61, 0x74,
+	0x75, 0x73, 0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x12, 0x34, 0x0a, 0x0a, 0x6f, 0x72,
+	0x64, 0x65, 0x72, 0x5f, 0x69, 0x74, 0x65, 0x6d, 0x18, 0x05, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x15,
+	0x2e, 0x6e, 0x6f, 0x77, 0x61, 0x2e, 0x4f, 0x72, 0x64, 0x65, 0x72, 0x2e, 0x4f, 0x72, 0x64, 0x65,
+	0x72, 0x49, 0x74, 0x65, 0x6d, 0x52, 0x09, 0x6f, 0x72, 0x64, 0x65, 0x72, 0x49, 0x74, 0x65, 0x6d,
+	0x1a, 0x54, 0x0a, 0x09, 0x4f, 0x72, 0x64, 0x65, 0x72, 0x49, 0x74, 0x65, 0x6d, 0x12, 0x2b, 0x0a,
+	0x09, 0x6d, 0x65, 0x6e, 0x75, 0x5f, 0x69, 0x74, 0x65, 0x6d, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b,
+	0x32, 0x0e, 0x2e, 0x6e, 0x6f, 0x77, 0x61, 0x2e, 0x4d, 0x65, 0x6e, 0x75, 0x49, 0x74, 0x65, 0x6d,
+	0x52, 0x08, 0x6d, 0x65, 0x6e, 0x75, 0x49, 0x74, 0x65, 0x6d, 0x12, 0x1a, 0x0a, 0x08, 0x71, 0x75,
+	0x61, 0x6e, 0x74, 0x69, 0x74, 0x79, 0x18, 0x02, 0x20, 0x01, 0x28, 0x05, 0x52, 0x08, 0x71, 0x75,
+	0x61, 0x6e, 0x74, 0x69, 0x74, 0x79, 0x22, 0x3f, 0x0a, 0x06, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73,
+	0x12, 0x0b, 0x0a, 0x07, 0x55, 0x4e, 0x4b, 0x4e, 0x4f, 0x57, 0x4e, 0x10, 0x00, 0x12, 0x0a, 0x0a,
+	0x06, 0x41, 0x43, 0x54, 0x49, 0x56, 0x45, 0x10, 0x01, 0x12, 0x0d, 0x0a, 0x09, 0x43, 0x4f, 0x4d,
+	0x50, 0x4c, 0x45, 0x54, 0x45, 0x44, 0x10, 0x02, 0x12, 0x0d, 0x0a, 0x09, 0x43, 0x41, 0x4e, 0x43,
+	0x45, 0x4c, 0x4c, 0x45, 0x44, 0x10, 0x03, 0x42, 0x20, 0x5a, 0x1e, 0x67, 0x69, 0x74, 0x68, 0x75,
+	0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x62, 0x6f, 0x6f, 0x6e, 0x65, 0x6e, 0x67, 0x2f, 0x6e, 0x6f,
+	0x77, 0x61, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x33,
 }
 
 var (
@@ -108,16 +471,27 @@ func file_protos_nowa_proto_rawDescGZIP() []byte {
 	return file_protos_nowa_proto_rawDescData
 }
 
-var file_protos_nowa_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_protos_nowa_proto_enumTypes = make([]protoimpl.EnumInfo, 2)
+var file_protos_nowa_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_protos_nowa_proto_goTypes = []interface{}{
-	(*Restaurant)(nil), // 0: nowa.Restaurant
+	(MenuItem_Status)(0),    // 0: nowa.MenuItem.Status
+	(Order_Status)(0),       // 1: nowa.Order.Status
+	(*Restaurant)(nil),      // 2: nowa.Restaurant
+	(*MenuItem)(nil),        // 3: nowa.MenuItem
+	(*Order)(nil),           // 4: nowa.Order
+	(*Order_OrderItem)(nil), // 5: nowa.Order.OrderItem
 }
 var file_protos_nowa_proto_depIdxs = []int32{
-	0, // [0:0] is the sub-list for method output_type
-	0, // [0:0] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	3, // 0: nowa.Restaurant.menu_items:type_name -> nowa.MenuItem
+	0, // 1: nowa.MenuItem.status:type_name -> nowa.MenuItem.Status
+	1, // 2: nowa.Order.status:type_name -> nowa.Order.Status
+	5, // 3: nowa.Order.order_item:type_name -> nowa.Order.OrderItem
+	3, // 4: nowa.Order.OrderItem.menu_item:type_name -> nowa.MenuItem
+	5, // [5:5] is the sub-list for method output_type
+	5, // [5:5] is the sub-list for method input_type
+	5, // [5:5] is the sub-list for extension type_name
+	5, // [5:5] is the sub-list for extension extendee
+	0, // [0:5] is the sub-list for field type_name
 }
 
 func init() { file_protos_nowa_proto_init() }
@@ -138,19 +512,56 @@ func file_protos_nowa_proto_init() {
 				return nil
 			}
 		}
+		file_protos_nowa_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*MenuItem); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_protos_nowa_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Order); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_protos_nowa_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Order_OrderItem); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_protos_nowa_proto_rawDesc,
-			NumEnums:      0,
-			NumMessages:   1,
+			NumEnums:      2,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
 		GoTypes:           file_protos_nowa_proto_goTypes,
 		DependencyIndexes: file_protos_nowa_proto_depIdxs,
+		EnumInfos:         file_protos_nowa_proto_enumTypes,
 		MessageInfos:      file_protos_nowa_proto_msgTypes,
 	}.Build()
 	File_protos_nowa_proto = out.File
